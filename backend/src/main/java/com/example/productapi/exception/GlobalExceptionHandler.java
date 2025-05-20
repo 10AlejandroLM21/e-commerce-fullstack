67e4bee29c19f200c74f7a2520b9ea27b7c40058
10AlejandroLM21 <ale.mts.jz@gmail.com>
2025-05-19 20:10:08 -0600

    fix: Corrección de errores en clase manejadora de errores(no retornaba nada)

--- backend/src/main/java/com/example/productapi/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/productapi/exception/GlobalExceptionHandler.java
@@ -1,4 +1,24 @@
 package com.example.productapi.exception;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
